706e579a96655d2f14b381236702d617cfc6d3db
MaRanda Robinson <ranrobin@onmail.com>
2023-07-05 16:06:29 -0400

    more css changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => console.log('All set!'))
 
 function fetchData(){
-    fetch('https://xivapi.com/Action/127')
-    .then((resp) => resp.json())
-    .then((data) => console.log(data))
+    fetch('https://xivapi.com/Action/127', {mode: 'cors'})
+    .then(resp => resp.json())
+    .then(data => console.log(data.Name_en))
 }
